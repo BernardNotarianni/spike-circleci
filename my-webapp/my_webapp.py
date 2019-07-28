15c7b07ea19386fd8ba4d3e46c4bf0f3806c1111
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-07-28 19:45:16 +0200

    change headline #12

--- my-webapp/my_webapp.py
+++ my-webapp/my_webapp.py
@@ -4,7 +4,7 @@ import my_domain
 @route('/')
 def index():
     message = my_domain.my_message()
-    return '<html><h1>Welcome to branch for issue 10</h1><img src="https://placekitten.com/200/300"/><p>{}</p></html>'.format(message)
+    return '<html><h1>This is a cat world!!</h1><img src="https://placekitten.com/200/300"/><p>{}</p></html>'.format(message)
 
 run(host='0.0.0.0', port=8000)
 
