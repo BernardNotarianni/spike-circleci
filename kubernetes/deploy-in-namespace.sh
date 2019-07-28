b98747343a401815c0aa340a54a6f0f8ced5c866
Bernard Notarianni <BernardNotarianni@users.noreply.github.com>
2019-07-28 19:33:37 +0200

    Tag namespaces (#14)
    * add step to deploy tags
    * create specific workflow for tags
    * introduce schell script to reduce copy/paste in circle ci

--- kubernetes/deploy-in-namespace.sh
+++ kubernetes/deploy-in-namespace.sh
@@ -1,5 +1,13 @@
-NAMESPACE=dev-1
+export TAG=$1
+export NAMESPACE=$2
 
-sed "s/___NAMESPACE___/$NAMESPACE/g" k8s_template.yml > k8s.yml
+echo ${GCLOUD_SERVICE_KEY} > gcp-key.json
+gcloud auth activate-service-account --key-file gcp-key.json
+gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials ${GOOGLE_CLUSTER_NAME}
 
-kubectl apply -f k8s.yml
+envsubst < k8s_template.yml > patched_k8s.yml
+
+kubectl apply -f patched_k8s.yml
+kubectl rollout status deployment/my-webapp -n ${NAMESPACE}
